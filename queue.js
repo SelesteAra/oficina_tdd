176cf48dcbbea88b7921d29b696188f4887bd6cb
seleste.Ara <selesteaz@gmail.com>
2025-05-10 19:20:19 -0300

    Criado teste, classes e funções

--- queue.js
+++ queue.js
@@ -3,8 +3,30 @@ class Queue {
   constructor(){
     this.elements = []
   }
+
+  size() {
+    return this.elements.length;
+  }
+
+  add(item) {
+    this.elements.push(item);
+  }
+
+  peek() {
+    if (this.elements.length === 0) {
+      return undefined;
+    }
+    return this.elements[0];
+  }
+
+  dequeue() {
+    if (this.elements.length === 0) {
+      return undefined;
+    }
+    return this.elements.shift();
+  }
   
-  // Implementar aqui
+ 
 }
 
 module.exports = Queue
--- soma.test.js
+++ soma.test.js
@@ -2,6 +2,7 @@ const soma = require('./soma')
 
 describe('Soma', () => {
   it('deverá somar dois números inteiros', () => {
+    expect(soma(2,3)).toBe(5)
     //seu código aqui
   })
 })
